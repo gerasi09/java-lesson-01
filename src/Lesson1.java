5f2d2489598009a5d7e5afe7298e999079549a64
Lauren Gerasimova <github@gerasimova.cz>
2024-04-01 00:46:17 +0200

    Lession #1, task #1 added

---
+++ src/Lesson1.java
@@ -0,0 +1,15 @@
+public class Lesson1 {
+
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Lesson 1, Task 1");
+        int b = 5;
+        int c = 6;
+        double a = 4 * (b + c - 1) /2d;
+        System.out.println("b = " + b + ", c = " + c + ", result a = " + a);
+    }
+
+}
